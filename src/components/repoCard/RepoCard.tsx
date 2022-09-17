9aa30827554a60f28ba866dc1f7d2342680f9451
Yash Falke <yashfalke77@gmail.com>
2022-09-17 20:28:26 +0530

    removing console logs

--- src/components/repoCard/RepoCard.tsx
+++ src/components/repoCard/RepoCard.tsx
@@ -15,8 +15,6 @@ function RepoCard({ name, description, topic, url }: RepoCardsProps) {
     const string = description ? description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ipsam voluptatibus asperiores, aperiam nostrum rem quisquam magni facere, cumque quam ut sapiente, impedit accusamus laboriosam optio esse sint deserunt eveniet!"
     const array = topic.length > 1 ? topic.slice(1, 5) : ["github"]
 
-    console.log(array)
-
 
     return (
         <div className='card'>
